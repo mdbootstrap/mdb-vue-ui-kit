c8423aabef1c3d59930de8b91e9539c511ce0f8b
poppabear8883 <servnx@gmail.com>
2017-12-30 15:47:23 -0500

    feat: Add basic video compatibility

--- src/components/ViewWrapper.vue
+++ src/components/ViewWrapper.vue
@@ -1,38 +1,60 @@
 <template>
   <div :is="tag" :class="className">
-    <img v-if="src" :src="src" :alt="alt" class="img-fluid">
+    <img v-if="src && !video" :src="src" :alt="alt" class="img-fluid">
+    <video v-if="src && video"
+           class="video-fluid"
+           :autoplay="autoplay ? 'true' : null"
+           :loop="loop ? 'true' : null">
+      <source :src="src" :type="videoType" />
+    </video>
     <slot></slot>
   </div>
 </template>
 
 <script>
-import classNames from 'classnames';
+  import classNames from 'classnames';
 
-export default {
-  props: {
-    tag: {
-      type: String,
-      default: "div"
+  export default {
+    props: {
+      tag: {
+        type: String,
+        default: "div"
+      },
+      overlay: {
+        type: String,
+      },
+      src: {
+        type: String
+      },
+      alt: {
+        type: String
+      },
+      video: {
+        type: Boolean,
+        default: false
+      },
+      videoType: {
+        type: String,
+        default: 'video/mp4'
+      },
+      autoplay: {
+        type: Boolean,
+        default: true
+      },
+      loop: {
+        type: Boolean,
+        default: true
+      }
     },
-    overlay: {
-      type: String,
-    },
-    src: {
-      type: String
-    },
-    alt: {
-      type: String
+    data() {
+      return {
+        className: classNames(
+          'view',
+          this.overlay ? 'hm-' + this.overlay : ''
+        ),
+      };
     }
-  },
-  data() {
-    return {
-      className: classNames(
-        'view',
-        this.overlay ? 'hm-' + this.overlay : ''
-      ),
-    };
-  }
-};
+  };
 </script>
 
 <style scoped>
