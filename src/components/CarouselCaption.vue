8e53313d77159f370674fe9ae43138de2d23e164
poppabear8883 <servnx@gmail.com>
2017-12-30 23:37:57 -0500

    feat: Add basic vertical positioning

--- src/components/CarouselCaption.vue
+++ src/components/CarouselCaption.vue
@@ -1,26 +1,53 @@
 <template>
-  <div :is="tag" class="carousel-caption">
+  <div :is="tag" :class="className">
     <h3 class="h3-responsive">{{ title }}</h3>
     <p>{{ text }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    tag: {
-      type: String,
-      default: "div"
+  import classNames from 'classnames';
+
+  export default {
+    props: {
+      tag: {
+        type: String,
+        default: "div"
+      },
+      title: {
+        type: String
+      },
+      text: {
+        type: String
+      },
+      alignV: {
+        type: String,
+        default: 'bottom'
+      }
     },
-    title: {
-      type: String
-    },
-    text: {
-      type: String
+    data() {
+      return {
+        className: classNames(
+          'carousel-caption',
+          `caption-${this.alignV}`
+        )
+      };
     }
-  },
-};
+  };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .carousel-caption {
+    &.caption-bottom {
+      bottom: 5% !important;
+    }
+
+    &.caption-center {
+      bottom: 35% !important;
+    }
+
+    &.caption-top {
+      bottom: 70% !important;
+    }
+  }
 </style>
